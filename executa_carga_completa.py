01cea9a46046b154bc67aedc99da470479e1bbe6
Caio Ruiz <cerz@gft.com>
2024-04-23 21:45:11 -0300

    removido pycharm

--- executa_carga_completa.py
+++ executa_carga_completa.py
@@ -4,7 +4,7 @@ from carga_completa import download_b3_pregoes, bronze_para_silver, seleciona_co
 
 
 if __name__ == '__main__':
-    # hoje = datetime.datetime.today()
-    # download_b3_pregoes(range(1986, hoje.year + 1))
-    # bronze_para_silver()
+    hoje = datetime.datetime.today()
+    download_b3_pregoes(range(1986, hoje.year + 1))
+    bronze_para_silver()
     seleciona_colunas_etf_precos()
